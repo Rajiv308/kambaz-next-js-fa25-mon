c6b4690387afc68a3ae7ed969a1b06cd6ee1460a
rpmenon <rajivpremnath308@gmail.com>
2025-09-22 08:31:46 -0400

    First commit Assignment 1

---
+++ app/(Kambaz)/Courses/[cid]/Assignments/[aid]/page.tsx
@@ -0,0 +1,154 @@
+export default function AssignmentEditor() {
+  return (
+    <div id="wd-assignments-editor">
+      <label htmlFor="wd-name">
+        <b>Assignment Name</b>
+      </label>
+      <br />
+      <br />
+      <input id="wd-name" defaultValue="A1 - ENV + HTML" />
+      <br />
+      <br />
+      <textarea id="wd-description" cols={50} rows={10}>
+        The assignment is available online. Submit a link to the landing page of
+        your Web application running on Vercel. The landing page should be the
+        Kambaz application with a link to the Lab exercises. Lab 1 should be the
+        landing page of the Lab exercises and should include the following: Your
+        full name and section Links to each of the lab assignments Link to the
+        Kambaz application Links to all relevant source code repositories The
+        Kambaz application should include a link to navigate back to the landing
+        page.
+      </textarea>
+      <br />
+      <br />
+      <table>
+        <tr>
+          <td align="right" valign="top">
+            <label htmlFor="wd-points">Points</label>
+          </td>
+          <td>
+            <input id="wd-points" defaultValue={100} />
+          </td>
+        </tr>
+        <br />
+        <tr>
+          <td align="right" valign="top">
+            <label htmlFor="wd-group">Assignment Group</label>
+          </td>
+          <td>
+            <select id="wd-group" defaultValue="ASSIGNMENTS">
+              <option value="ASSIGNMENTS">Assignments</option>
+              <option value="QUIZZES">Quizzes</option>
+              <option value="EXAMS">Exams</option>
+              <option value="PROJECT">Project</option>
+            </select>
+          </td>
+        </tr>
+        <br />
+        <tr>
+          <td align="right" valign="top">
+            <label htmlFor="wd-display-grade-as">Display Grade as</label>
+          </td>
+          <td>
+            <select id="wd-display-grade-as" defaultValue="PERCENTAGE">
+              <option value="PERCENTAGE">Percentage</option>
+              <option value="CGPA">CGPA</option>
+              <option value="ABSOLUTE">Absolute</option>
+            </select>
+          </td>
+        </tr>
+        <br />
+        <tr>
+          <td align="right" valign="top">
+            <label htmlFor="wd-submission-type">Submission Type</label>
+          </td>
+          <td>
+            <select id="wd-submission-type" defaultValue="ONLINE">
+              <option value="ONLINE">Online</option>
+              <option value="WRITTEN">Written</option>
+              <option value="PRESENTATION">Presentation</option>
+            </select>
+            <br />
+            <br />
+            <label>Online Entry Options:</label>
+            <br />
+            <input type="checkbox" name="wd-online-entry" id="wd-text-entry" />
+            <label htmlFor="wd-text-entry">Text Entry</label>
+            <br />
+            <input type="checkbox" name="wd-online-entry" id="wd-website-url" />
+            <label htmlFor="wd-website-url">Website URL</label>
+            <br />
+            <input
+              type="checkbox"
+              name="wd-online-entry"
+              id="wd-media-recordings"
+            />
+            <label htmlFor="wd-media-recordings">Media Recordings</label>
+            <br />
+            <input
+              type="checkbox"
+              name="wd-online-entry"
+              id="wd-student-annotation"
+            />
+            <label htmlFor="wd-student-annotation">Student Annotations</label>
+            <br />
+            <input type="checkbox" name="wd-online-entry" id="wd-file-upload" />
+            <label htmlFor="wd-file-upload">File Upload</label>
+          </td>
+        </tr>
+        <br />
+        <tr>
+          <td align="right" valign="top">
+            <label htmlFor="wd-assign-to">Assign</label>
+          </td>
+          <td>
+            <label htmlFor="wd-assign-to">Assign To</label>
+            <br />
+            <input id="wd-assign-to" defaultValue="Everyone" />
+          </td>
+        </tr>
+        <br />
+        <tr>
+          <td align="right" valign="top"></td>
+          <td>
+            <label htmlFor="wd-due-date">Due</label>
+            <br />
+            <input type="date" defaultValue="20225-10-21" id="wd-due-date" />
+          </td>
+        </tr>
+        <br />
+        <tr>
+          <td align="right" valign="top"></td>
+          <table>
+            <tr>
+              <td>
+                <label htmlFor="wd-available-from">Available From</label>
+                <br />
+                <input
+                  type="date"
+                  defaultValue="2025-10-21"
+                  id="wd-available-from"
+                />
+              </td>
+              <td>
+                <label htmlFor="wd-available-until">Until</label>
+                <br />
+                <input
+                  type="date"
+                  defaultValue="2025-10-21"
+                  id="wd-available-until"
+                />
+              </td>
+            </tr>
+          </table>
+        </tr>
+        <br />
+      </table>
+      <hr />
+      <div style={{ textAlign: "right" }}>
+        <button id="wd-cancel">Cancel</button>
+        <button id="wd-save">Save</button>
+      </div>
+    </div>
+  );
+}
---
+++ app/(Kambaz)/Dashboard/page.tsx
@@ -0,0 +1,138 @@
+import Link from "next/link";
+import Image from "next/image";
+export default function Dashboard() {
+  return (
+    <div id="wd-dashboard">
+      <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
+      <h2 id="wd-dashboard-published">Published Courses (12)</h2> <hr />
+      <div id="wd-dashboard-courses">
+        <div className="wd-dashboard-course">
+          <Link href="/Courses/1234" className="wd-dashboard-course-link">
+            <Image
+              src="/images/reactjs.jpg"
+              alt="React JS course thumbnail"
+              width={200}
+              height={150}
+            />
+            <div>
+              <h5> CS1234 React JS </h5>
+              <p className="wd-dashboard-course-title">
+                Full Stack software developer
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+
+        <div className="wd-dashboard-course">
+          <Link href="/Courses/5501" className="wd-dashboard-course-link">
+            <Image
+              src="/images/ai.jpg"
+              alt="Artificial Intelligence course thumbnail"
+              width={200}
+              height={150}
+            />
+            <div>
+              <h5> CS5501 Artificial Intelligence & Machine Learning </h5>
+              <p className="wd-dashboard-course-title">
+                Learn the foundations of AI, ML algorithms, and applications.
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+
+        <div className="wd-dashboard-course">
+          <Link href="/Courses/5502" className="wd-dashboard-course-link">
+            <Image
+              src="/images/dsa.jpg"
+              alt="Data Structures and Algorithm course thumbnail"
+              width={200}
+              height={150}
+            />
+            <div>
+              <h5> CS5502 Data Structures & Algorithms </h5>
+              <p className="wd-dashboard-course-title">
+                Master problem-solving with efficient algorithms and data
+                models.
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+
+        <div className="wd-dashboard-course">
+          <Link href="/Courses/5503" className="wd-dashboard-course-link">
+            <Image
+              src="/images/os.jpg"
+              alt="Operating Systems course thumbnail"
+              width={200}
+              height={150}
+            />
+            <div>
+              <h5> CS5503 Operating Systems & Virtualization </h5>
+              <p className="wd-dashboard-course-title">
+                Explore processes, memory, and virtualization technologies.
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+
+        <div className="wd-dashboard-course">
+          <Link href="/Courses/5504" className="wd-dashboard-course-link">
+            <Image
+              src="/images/dbms.jpg"
+              alt="Database Management Systems course thumbnail"
+              width={200}
+              height={150}
+            />
+            <div>
+              <h5> CS5504 Database Management Systems </h5>
+              <p className="wd-dashboard-course-title">
+                Understand relational models, SQL, and transaction systems.
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+
+        <div className="wd-dashboard-course">
+          <Link href="/Courses/5505" className="wd-dashboard-course-link">
+            <Image
+              src="/images/networks.jpg"
+              alt="Computer Networks and Security course thumbnail"
+              width={200}
+              height={150}
+            />
+            <div>
+              <h5> CS5505 Computer Networks & Security </h5>
+              <p className="wd-dashboard-course-title">
+                Dive into TCP/IP, routing, and cybersecurity principles.
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+
+        <div className="wd-dashboard-course">
+          <Link href="/Courses/5506" className="wd-dashboard-course-link">
+            <Image
+              src="/images/hci.jpg"
+              alt="Human-Computer Interaction course thumbnail"
+              width={200}
+              height={150}
+            />
+            <div>
+              <h5> CS5506 Human–Computer Interaction </h5>
+              <p className="wd-dashboard-course-title">
+                Design usable, accessible, and engaging user interfaces.
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
